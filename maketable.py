89482b0168ddf13286d3a6071f799c226aaad80d
Titus Nicolae <nicolaetitus12@gmail.com>
2011-06-19 10:48:27 +0300

    first commit

---
+++ maketable.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+#creates html table from space separated values
+import sys
+def createHTMLtableFromFile():
+  if len(sys.argv)==1:
+    print "missing input and output files"
+    return 
+  elif len(sys.argv)==2:
+    print "missing output file"
+    
+  fin = open(sys.argv[1])
+  out=['<table>\n']
+  for line in fin:
+    tr=["<tr>\n"]
+    for el in line.split():
+      tr.append("<td> %s </td>\n" % (el))
+    tr.append("</tr>\n")
+    out+=tr
+  out.append("</table>")
+  fout=open(sys.argv[2],"w")
+  fout.write(''.join(out))
+ 
+if __name__ == "__main__":
+  createHTMLtableFromFile()
